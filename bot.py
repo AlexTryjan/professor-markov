56f8e545bcc009899d3cedf4875b59707e8acc88
AlexTryjan <aet41@case.edu>
2016-12-31 20:30:02 -0500

    Tweepy & Markovify Switch
    Signed-off-by: AlexTryjan <aet41@case.edu>

--- bot.py
+++ bot.py
@@ -1,16 +1,28 @@
 import os
 import pickle
-from markovbot import MarkovBot
+import tweepy
 from secret import *
 import time
+import random
+import markovify
 
-bot = MarkovBot()
+# Get raw text as string.
+with open("descriptions.txt") as f:
+    text = f.read()
+# Build the model.
+text_model = markovify.Text(text)
+# Get pokemon names as list
+with open('names.txt', 'rb') as f:
+    pkmn_names = pickle.load(f)
+# tweepy authentication
+auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+api = tweepy.API(auth)
 
-dirname = os.path.dirname(os.path.abspath(__file__))
-source = os.path.join(dirname, 'descriptions.txt')
-bot.read(source)
-
-bot.twitter_login(CONSUMER_KEY, CONSUMER_SECRET, ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
-bot.twitter_tweeting_start(days=0, hours=12, minutes=00, keywords=None, prefix=None, suffix=None)
-time.sleep(30)
-bot.twitter_tweeting_stop()
+# Tweeting Loop
+while True:
+    tweet = text_model.make_short_sentence(115)
+    rand_pkmn = random.randrange(0, 800)
+    tweet_with_pkmn = "%s - %s" % (pkmn_names[rand_pkmn], tweet)
+    api.update_status(tweet_with_pkmn)
+    time.sleep(43200) #wait 12 hours between tweets
