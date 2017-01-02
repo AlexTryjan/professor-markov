e9638e7519b779e794c251c5385b52f5f179fd34
AlexTryjan <aet41@case.edu>
2017-01-02 18:47:11 -0500

    MarkovBot Re-added
    Signed-off-by: AlexTryjan <aet41@case.edu>

--- bot.py
+++ bot.py
@@ -1,16 +1,14 @@
 import os
 import pickle
 import tweepy
+from markovbot import MarkovBot
 from secret import *
 import time
 import random
-import markovify
 
 # Get raw text as string.
 with open("descriptions.txt") as f:
     text = f.read()
-# Build the model.
-text_model = markovify.Text(text)
 # Get pokemon names as list
 with open('names.txt', 'rb') as f:
     pkmn_names = pickle.load(f)
@@ -18,11 +16,22 @@ with open('names.txt', 'rb') as f:
 auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
 api = tweepy.API(auth)
+# MarkovBot creation
+bot = MarkovBot()
+dirname = os.path.dirname(os.path.abspath(__file__))
+source = os.path.join(dirname, 'descriptions.txt')
+bot.read(source)
 
 # Tweeting Loop
 while True:
-    tweet = text_model.make_short_sentence(115)
+    # Generate random pkmn name
     rand_pkmn = random.randrange(0, 800)
-    tweet_with_pkmn = "%s - %s" % (pkmn_names[rand_pkmn], tweet)
+    pkmn = pkmn_names[rand_pkmn]
+    # Generate tweet containing at most 20 words (checks for max tweet length)
+    tweet = bot.generate_text(20)
+    while len(tweet) > 120:
+        tweet = bot.generate_text(20)
+    tweet_with_pkmn = "%s - %s" % (pkmn, tweet)
+    # Publish tweet
     api.update_status(tweet_with_pkmn)
     time.sleep(43200) #wait 12 hours between tweets
